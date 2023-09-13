6c50748f2231cffdfafcc0c400511beb6ebd0434
Billy Duraney <billy@triplewhale.com>
2023-09-13 09:46:18 -0400

    move logs

--- us-central1/run_npm.sh
+++ us-central1/run_npm.sh
@@ -2,20 +2,20 @@
 
 npm install --timing @tw/cli
 
-DEBUG_LOG=`ls /root/.npm/_logs/*-debug*.log | tail -n 1`
-TIMING_LOG=`ls /root/.npm/_logs/*-timing*.log | tail -n 1`
-cp $LOGFILE /root/us-central1-first-run-debug.log
-cp $TIMING_LOG /root/us-central1-first-run-timing.log
+DEBUG_LOG=$( ls ./.npm/_logs/*-debug*.log | tail -n 1 )
+TIMING_LOG=$( ls ./.npm/_logs/*-timing*.json | tail -n 1 )
+cp $DEBUG_LOG /root/us-central1-first-run-debug.log
+cp $TIMING_LOG /root/us-central1-first-run-timing.json
 
 rm -rf /root/node_modules
 rm -rf /root/package*
 
 npm install --timing @tw/cli
 
-DEBUG_LOG=`ls /root/.npm/_logs/*-debug*.log | tail -n 1`
-TIMING_LOG=`ls /root/.npm/_logs/*-timing*.log | tail -n 1`
-cp $LOGFILE /root/us-central1-second-run-debug.log
-cp $TIMING_LOG /root/us-central1-second-run-timing.log
+DEBUG_LOG=$( ls ./.npm/_logs/*-debug*.log | tail -n 1 )
+TIMING_LOG=$( ls ./.npm/_logs/*-timing*.json | tail -n 1 )
+cp $DEBUG_LOG /root/us-central1-second-run-debug.log
+cp $TIMING_LOG /root/us-central1-second-run-timing.json
 
 rm -rf /root/node_modules
 rm -rf /root/package*
--- us/run_npm.sh
+++ us/run_npm.sh
@@ -2,20 +2,20 @@
 
 npm install --timing @tw/cli@0.1.91-beta.2
 
-DEBUG_LOG=`ls /root/.npm/_logs/*-debug*.log | tail -n 1`
-TIMING_LOG=`ls /root/.npm/_logs/*-timing*.log | tail -n 1`
-cp $LOGFILE /root/us-first-run-debug.log
-cp $TIMING_LOG /root/us-first-run-timing.log
+DEBUG_LOG=$( ls ./.npm/_logs/*-debug*.log | tail -n 1 )
+TIMING_LOG=$( ls ./.npm/_logs/*-timing*.json | tail -n 1 )
+cp $DEBUG_LOG ./us-first-run-debug.log
+cp $TIMING_LOG /root/us-first-run-timing.json
 
 rm -rf /root/node_modules
 rm -rf /root/package*
 
 npm install --timing @tw/cli@0.1.91-beta.2
 
-DEBUG_LOG=`ls /root/.npm/_logs/*-debug*.log | tail -n 1`
-TIMING_LOG=`ls /root/.npm/_logs/*-timing*.log | tail -n 1`
-cp $LOGFILE /root/us-second-run-debug.log
-cp $TIMING_LOG /root/us-second-run-timing.log
+DEBUG_LOG=$( ls ./.npm/_logs/*-debug*.log | tail -n 1 )
+TIMING_LOG=$( ls ./.npm/_logs/*-timing*.json | tail -n 1 )
+cp $DEBUG_LOG /root/us-second-run-debug.log
+cp $TIMING_LOG /root/us-second-run-timing.json
 
 rm -rf /root/node_modules
 rm -rf /root/package*
