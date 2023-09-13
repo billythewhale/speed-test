1251f3dfc7816a0b7636118fae4619d595df1458
Billy Duraney <billy@triplewhale.com>
2023-09-13 10:15:33 -0400

    update script to be more better

--- us-central1/run_npm.sh
+++ us-central1/run_npm.sh
@@ -4,6 +4,8 @@ npm install --timing @tw/cli
 
 DEBUG_LOG=$( ls ./.npm/_logs/*-debug-0.log | tail -n 1 )
 TIMING_LOG=$( ls ./.npm/_logs/*-timing.json | tail -n 1 )
+echo "Debug log is $DEBUG_LOG"
+echo "Timing log is $TIMING_LOG"
 cp $DEBUG_LOG /root/us-central1-first-run-debug.log
 cp $TIMING_LOG /root/us-central1-first-run-timing.json
 
@@ -14,6 +16,8 @@ npm install --timing @tw/cli
 
 DEBUG_LOG=$( ls ./.npm/_logs/*-debug-0.log | tail -n 1 )
 TIMING_LOG=$( ls ./.npm/_logs/*-timing.json | tail -n 1 )
+echo "Debug log is $DEBUG_LOG"
+echo "Timing log is $TIMING_LOG"
 cp $DEBUG_LOG /root/us-central1-second-run-debug.log
 cp $TIMING_LOG /root/us-central1-second-run-timing.json
 
--- us/run_npm.sh
+++ us/run_npm.sh
@@ -2,8 +2,10 @@
 
 npm install --timing @tw/cli@0.1.91-beta.2
 
-DEBUG_LOG=$( ls ./.npm/_logs/*-debug-0.log | tail -n 1 )
-TIMING_LOG=$( ls ./.npm/_logs/*-timing.json | tail -n 1 )
+DEBUG_LOG=`ls ./.npm/_logs/*-debug-0.log | tail -n 1`
+TIMING_LOG=`ls ./.npm/_logs/*-timing.json | tail -n 1`
+echo "Debug log is $DEBUG_LOG"
+echo "Timing log is $TIMING_LOG"
 cp $DEBUG_LOG ./us-first-run-debug.log
 cp $TIMING_LOG /root/us-first-run-timing.json
 
@@ -12,8 +14,10 @@ rm -rf /root/package*
 
 npm install --timing @tw/cli@0.1.91-beta.2
 
-DEBUG_LOG=$( ls ./.npm/_logs/*-debug-0.log | tail -n 1 )
-TIMING_LOG=$( ls ./.npm/_logs/*-timing.json | tail -n 1 )
+DEBUG_LOG=`ls ./.npm/_logs/*-debug-0.log | tail -n 1`
+TIMING_LOG=`ls ./.npm/_logs/*-timing.json | tail -n 1`
+echo "Debug log is $DEBUG_LOG"
+echo "Timing log is $TIMING_LOG"
 cp $DEBUG_LOG /root/us-second-run-debug.log
 cp $TIMING_LOG /root/us-second-run-timing.json
 
